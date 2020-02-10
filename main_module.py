9d3c9e4e0b811d120eb44f3118d39555776ac252
dipeshsm <dipeshsm@yahoo.com>
2020-02-10 15:36:31 -0800

    Create main_module.py

---
+++ main_module.py
@@ -0,0 +1,9 @@
+import #module1
+import #module2
+import #module3
+
+
+filename='~/Documents/wiki.txt'
+file_object  = open('/Users/dmehta1/Documents/wiki.txt', 'r')
+for i in file_object:
+    print(i)
