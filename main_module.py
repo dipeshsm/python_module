540ead7fb5d1eb8f7c7ace7baed3fc339bce5ab8
dipeshsm <dipeshsm@yahoo.com>
2020-02-10 16:10:15 -0800

    Update main_module.py

--- main_module.py
+++ main_module.py
@@ -1,4 +1,4 @@
-import #module1
+import Gaurav.py as gv
 import #module2
 import #module3
 
@@ -7,3 +7,5 @@ filename='~/Documents/wiki.txt'
 file_object  = open('/Users/dmehta1/Documents/wiki.txt', 'r')
 for i in file_object:
     print(i)
+    
+gv.MaxWordLen(filename)
